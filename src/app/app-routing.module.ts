c12938ea646901832701d9367a83ca4496f9d497
Kevin Silva <kevincaprino33@gmail.com>
2023-09-27 21:30:10 -0300

    Rota para verificar emails

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { LoginComponent } from './auth/login/login.component';
 import { AuthGuard } from './services/auth/auth.guard';
 import { RegisterComponent } from './system/register/register.component';
 import { EmailVerificationGuard } from './services/auth/email-verification.guard';
+import { EmailValidationComponent } from './auth/email-validation/email-validation/email-validation.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path:'home', component: HomeComponent },
+  { path:'home', component: HomeComponent, canActivate: [EmailVerificationGuard] },
   { path:'auth/login', component: LoginComponent },
   { path:'register', component: RegisterComponent },
+  { path:'verify-email', component: EmailValidationComponent },
 ];
 
 @NgModule({

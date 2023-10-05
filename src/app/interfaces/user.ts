1beb36ccce2dc9f54c0f43bf9bd3381b4f745aea
Kevin Silva <kevincaprino33@gmail.com>
2023-10-04 22:03:18 -0300

    Interface de usuarios

---
+++ src/app/interfaces/user.ts
@@ -0,0 +1,20 @@
+export interface User {
+    id: number;
+    name: string;
+    email: string;
+    document: string;
+    phone?: string;
+    phone_whatsapp?: string;
+    link_instagram?: string;
+    link_facebook?: string;
+    state?: string;
+    city?: string;
+    neighborhood?: string;
+    street?: string;
+    number?: string;
+    complement?: string;
+    archived: boolean;
+    user_type: number;
+    created_at: Date;
+    updated_at: Date;
+}

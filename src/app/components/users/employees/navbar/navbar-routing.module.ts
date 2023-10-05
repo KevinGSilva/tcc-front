6b0dc679c919cf10ab5c864c0b6598af5baa3e0f
Kevin Silva <kevincaprino33@gmail.com>
2023-10-04 22:02:17 -0300

    Barra de navegacao para usuarios logados

---
+++ src/app/components/users/employees/navbar/navbar-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ProfileComponent } from '../profile/profile.component';
+import { EmailVerificationGuard } from 'src/app/services/auth/email-verification.guard';
+import { NavbarComponent } from './navbar.component';
+import { AuthGuard } from 'src/app/services/auth/auth.guard';
+
+const routes: Routes = [
+
+  {path: 'employee', component: NavbarComponent, children: [
+    {path: '', redirectTo: 'profile', pathMatch: 'full'},
+    {path: 'profile', component: ProfileComponent, canActivate: [ AuthGuard ,EmailVerificationGuard]},
+  ]},
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class NavbarRoutingModule { }
---
+++ src/app/components/users/employees/navbar/navbar.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { NavbarRoutingModule } from './navbar-routing.module';
+import { ProfileComponent } from '../profile/profile.component';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    NavbarRoutingModule
+  ]
+})
+export class NavbarModule { }

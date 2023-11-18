d01affc2a3703f5836d817734de3cbcf16bf2be3
Kevin Silva <kevincaprino33@gmail.com>
2023-11-18 04:17:50 -0300

    Filtrar por cidade e mostrar avaliacao geral de prestadores

--- src/app/components/users/contractors/index/index.module.ts
+++ src/app/components/users/contractors/index/index.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IndexComponent } from './index.component';
 import { FormsModule } from '@angular/forms';
+import { StarRatingModule } from 'angular-star-rating';
 
 
 
@@ -11,7 +12,8 @@ import { FormsModule } from '@angular/forms';
   ],
   imports: [
     CommonModule,
-    FormsModule
+    FormsModule,
+    StarRatingModule,
   ]
 })
 export class IndexModule { }
